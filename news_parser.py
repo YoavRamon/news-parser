c558d58d81862a502df8bd17476cd5e79765f493
YoavRamon <Yoav@Sensiya.com>
2018-12-05 16:19:57 +0200

    First commit

---
+++ news_parser.py
@@ -0,0 +1,57 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import logging
+import time
+import sys
+
+
+class NewsLogger:
+    def __init__(self, mode):
+        self.mode = mode
+        self._initialize_logger()
+
+    def _initialize_logger(self):
+        formatter = logging.Formatter('%(asctime)s - %(message)s')
+        fh = logging.FileHandler(self.mode + '.log', 'a')
+        fh.setFormatter(formatter)
+
+        self.logger = logging.getLogger(self.mode + '_app')
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(fh)
+
+    def log_site(self):
+        if self.mode == 'ynet':
+            html = request.urlopen('https://www.ynet.co.il/home/0,7340,L-8,00.html')
+            soup = BeautifulSoup(html, 'html.parser')
+
+            for a in soup.find_all('span', attrs={'class': 'subtitle'}):
+                text = a.text.strip()
+                self.logger.info(text)
+            for a in soup.find_all('div', attrs={'class': 'str3s_txt'}):
+                text = [b for b in a.children][0].text.strip()
+                self.logger.info(text)
+
+    def log_in_interval(self, sleep_time):
+        # Using sleep_time < 60 might be considered as an attack
+        while True:
+            self.log_site()
+            print('logging ' + self.mode)
+            time.sleep(sleep_time)
+
+
+if __name__ == '__main__':
+    args_num = len(sys.argv)
+    if args_num == 3:
+        ynet_logger = NewsLogger(sys.argv[1])
+        ynet_logger.log_in_interval(int(sys.argv[2]))
+    else:
+        print('usage:')
+        print('news_parser.py <parser_mode> <logging_interval>')
+        print('<parser_mode>        the name of the site you want to log')
+        print('<logging_interval>   the interval of logging the site (in seconds)\n')
+        print('for example:')
+        print('* python news_parser.py ynet 100\n')
+        print('available parsers:')
+        print('* ynet - www.ynet.co.il\n')
+        print('Important notes:')
+        print('* Please don\'t use logging_interval < 60')
---
+++ readme.md
@@ -0,0 +1,31 @@
+# News Parser
+This tool allows you to log the main titles of news sites in Israel.
+
+The implementation is based on [bs4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/).
+
+## Installation
+This tool is based on Python version 3.5 and above.
+
+to use the tool install the requirements file with pip in the following way:
+```
+pip install -r requirements.txt 
+```
+
+## Usage
+for basic usage you can use news_parser.py as an command line tool. 
+for example, if you'll want to scarp the titles of ynet every 60 seconds you could use:
+```
+python news_parser.py ynet 60
+```
+Note: Please don't use any time that is less than 60 seconds or it might being consider as an attack by the news site.
+
+### Available modes
+1. ynet - logs [ynet](https://www.ynet.co.il/home/0,7340,L-8,00.html)
+
+Legal notes
+---------------------------------------
+This tool is considered a [web scarper](https://en.wikipedia.org/wiki/Web_scraping), 
+please use it in an [ethical](https://towardsdatascience.com/ethics-in-web-scraping-b96b18136f01) way 
+AND according to the [Israeli law](http://legalstart.idc.ac.il/he/web-scraping-%D7%91%D7%93%D7%99%D7%9F-%D7%94%D7%99%D7%A9%D7%A8%D7%90%D7%9C%D7%99/)
+
+I'm not responsible for any misusing of that tool.
